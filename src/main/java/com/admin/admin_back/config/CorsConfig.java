cf7e06505e344383b9a0ec3bdeaec9b4adb6d120
Zrzi <201900301041@mail.sdu.edu.cn>
2023-02-20 21:08:33 +0800

    '配置CorsConfig'

--- src/main/java/com/admin/admin_back/config/CorsConfig.java
+++ src/main/java/com/admin/admin_back/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.admin.admin_back.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,32 +9,43 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// 由nginx代理，这里不需要跨域配置
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    private CorsConfiguration buildConfig() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOrigin("*");
-//        corsConfiguration.addAllowedHeader("*");
-//        corsConfiguration.addAllowedMethod("*");
-//        corsConfiguration.addExposedHeader("Authorization");
-//        return corsConfiguration;
+//import javax.annotation.PostConstruct;
+
+/**
+ * 当服务器由nginx代理时，不需要配置跨域
+ * @author 陈群矜
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "admin", name = "corsConfig", havingValue = "true", matchIfMissing = false)
+public class CorsConfig implements WebMvcConfigurer {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addExposedHeader("Authorization");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+
+//    @PostConstruct
+//    public void post() {
+//        System.out.println("CorsConfig起效");
 //    }
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", buildConfig());
-//        return new CorsFilter(source);
-//    }
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST", "DELETE", "PUT")
-//                .maxAge(3600);
-//    }
-//
-//}
+
+}
