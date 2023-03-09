943a26b8ff12579c58f6c27f06ec34433fde1682
Zrzi <201900301041@mail.sdu.edu.cn>
2023-03-09 12:53:03 +0800

    '测试提交数值类型'

--- src/main/resources/sql/test.sql
+++ src/main/resources/sql/test.sql
@@ -4,5 +4,8 @@ CREATE TABLE `admin_test` (
     `test_id_1` VARCHAR(40) NOT NULL COMMENT '编码1',
     `test_id_2` VARCHAR(40) NOT NULL COMMENT '编码2',
     `test_text` TEXT NOT NULL COMMENT '内容',
+    `test_number` int NOT NULL COMMENT '数字',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `admin_test` ADD COLUMN `test_number` int NOT NULL COMMENT '数字';
